bc8a26ea1afe75a5a42f6463f31a9ef8676fd2ed
mindy <mindy-leaderGit>
2020-11-24 11:21:59 +0200

    initial commit

---
+++ src/components/ConversationListItem/index.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import shave from 'shave';
+import ConvesationsService from '../../services/conversations.service'
+import './ConversationListItem.css';
+
+export default function ConversationListItem(props) {
+  useEffect(() => {
+    shave('.conversation-snippet', 20);
+  })
+  const activateLasers=async()=>{
+    alert(_id)
+    const listConvesation = await ConvesationsService.getHangoutByID(_id);
+
+
+
+  
+    // for(i=0;i<listConvesation.length;i++){
+    //         alert(listConvesation[i])
+    // }
+
+ 
+
+}
+  
+  const {_id, profileGroup, name, text } = props.data;
+
+  return (
+    <div className="conversation-list-item" onClick={activateLasers} >
+      <img className="conversation-photo" src={require("../../images/"+profileGroup)} alt="conversation" />
+      {/* <img className="conversation-photo" src={require('../../images/user-1.jpg')} alt="conversation" />  */}
+
+      <div className="conversation-info">
+        <h1 className="conversation-title">{name}</h1>
+        <p className="conversation-snippet">{text}</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import {creatStore} from 'redux';
+import produce from 'immer';
+
+
+const initalStaste={
+     listConvesation:{}
+  
+}
+
+export const store = createStore(listConvesation, {})
