7f5ab5320705fbbc44bba5faeb2b513411e24cec
chavaElyashiv <chavae@leader.codes>
2021-01-18 14:08:21 +0200

    socket and prevent user from link itsef to crm

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -52,7 +52,7 @@ export default connect(mapStateToProps, mapDispatchToProps)(function Message(pro
           {/* <p class="sentText pr-10">{name}</p> */}
         </div>
         {/* `https://contacts.leader.codes/${userName}` */}
-        <a href="#"onClick={(e)=>{func(); return false;}} class="active">
+        <a href="#" onClick={(e) => { func(); return false; }} class="active">
 
           <div class="from2" >{data.from}</div> </a>
 
@@ -83,11 +83,11 @@ export default connect(mapStateToProps, mapDispatchToProps)(function Message(pro
             {/* <div>{data.from}</div>   */}
 
           </div>
-          <a href="#"onClick={(e)=>{func(); return false;}} class="active">
+          {/* <a href="#"onClick={(e)=>{func(); return false;}} class="active">
 
-            <div class="from" >{data.from}</div> </a>
+            <div class="from" >{data.from}</div> </a> */}
 
-          {/* <div  class="from" >{data.from}</div>  */}
+          <div class="from" >ME</div>
 
         </div>
 
